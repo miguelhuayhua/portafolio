b2bff5f9e4a373e063f37874ae50320af9977a0a
miguelhuayhua <miguelhuayhuac2@gmail.com>
2023-09-23 21:49:58 -0400

    configurando next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-
 module.exports = {
-    env: {
-    },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.node/,
-        use: 'raw-loader'
-      })
-      return config;
-    }
-  };
+  env: {},
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+  output: "export",
+};
